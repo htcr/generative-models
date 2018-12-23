c0eed03dd404aae5e8f605f4329b2f5f4c00362c
htcr <congruihetang@gmail.com>
2018-12-23 15:49:02 -0500

    added encoder

--- vae.py
+++ vae.py
@@ -4,6 +4,9 @@ import torchvision
 import torchvision.transforms as transforms
 import os
 import matplotlib.pyplot as plt
+import torch.nn as nn
+from torch.nn import Module
+import torch.nn.functional as F
 
 data_root = './data'
 if not os.path.exists(data_root):
@@ -24,7 +27,39 @@ train_loader = torch.utils.data.DataLoader(
     dataset=train_dataset, batch_size=train_batch_size, 
     shuffle=True)
 
+
+class Encoder(Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        self.input_size = 28**2
+        self.hidden_size = 300
+        self.latent_size = 2
+
+        self.fc1 = nn.Linear(
+            in_features=self.input_size, 
+            out_features=self.hidden_size
+        )
+        self.fc_mu = nn.Linear(
+            in_features=self.hidden_size, 
+            out_features=self.latent_size
+        )
+        self.fc_sigma = nn.Linear(
+            in_features=self.hidden_size,
+            out_features=self.latent_size
+        )
+
+    def forward(self, x):
+        x = x.view(x.shape[0], -1)
+        x = F.relu(self.fc1(x))
+        mu = self.fc_mu(x)
+        sigma = self.fc_sigma(x)
+        return mu, sigma
+
+
 for data, label in train_loader:
+    pass
+
+    '''
     print('datatype', type(data))
     print('labeltype', type(label))
     print('datashape', data.shape)
@@ -32,3 +67,4 @@ for data, label in train_loader:
 
     plt.imshow(data[0, 0, :, :])
     plt.show()
+    '''
